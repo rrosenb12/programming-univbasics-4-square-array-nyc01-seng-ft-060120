62b8c0858bd4843190aef068bf03fa70b596fdde
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-04-23 14:42:31 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   array = [1,2,3]
   array.length.times do |num|
-    square_array.push(numbers[num] ** 2)
+    square_array.push(array[num] ** 2)
   end
   square_array
 end
