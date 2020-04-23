807a26b7ac11bf238b561b90bdd9336bf846d45c
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-04-23 14:51:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,8 @@
 def square_array(array)
   numbers = [1,2,3]
   new_numbers = []
-  count = 0
-  while numbers[counter] do |num|
+  numbers.length.times do |num|
     new_numbers.push(numbers[num] ** 2)
   end
-  count += 1
   new_numbers
 end
